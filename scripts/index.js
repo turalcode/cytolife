44f3228806d972d957a8f7f308cfd95ce092757e
turalcode <turalcodechannel@gmail.com>
2025-09-12 19:30:38 +0300

    верстка главной страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
 // document.body.hidden = true // test
+const swiperProducts = new Swiper('.swiper-products', {
+  // Optional parameters
+  slidesPerView: "3",
+  spaceBetween: 20,
+  // loop: true,
+});
