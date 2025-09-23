5c09902023c96d0bda328a80eaf8cf7c09d4e09e
turalcode <turalcodechannel@gmail.com>
2025-09-23 22:02:34 +0300

    Верстка главной страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,86 +1,125 @@
 "use strict";
 
-// SWIPER SLIDER
+document.addEventListener("DOMContentLoaded", () => {
+  // SWIPER SLIDER
 
-const swiperProducts = new Swiper(".swiper-products", {
-  slidesPerView: 1.1,
-  spaceBetween: 10,
+  const swiperProducts = new Swiper(".swiper-products", {
+    slidesPerView: 1.1,
+    spaceBetween: 10,
 
-  breakpoints: {
-    567: {
-      spaceBetween: 20,
+    breakpoints: {
+      567: {
+        spaceBetween: 20,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1199: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
     },
-    767: {
-      slidesPerView: 2,
-    },
-    1199: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-const swiperCertificate = new Swiper(".swiper-certificate", {
-  slidesPerView: 2.5,
-  spaceBetween: 10,
-
-  breakpoints: {
-    567: {
-      spaceBetween: 20,
-      slidesPerView: 2.5,
-    },
-    767: {
-      spaceBetween: 20,
-      slidesPerView: 3.5,
-    },
-    991: {
-      spaceBetween: 20,
-      slidesPerView: 4.5,
-    },
-  },
-});
-
-const swiperTabs = new Swiper(".swiper-tabs", {
-  slidesPerView: "auto",
-  spaceBetween: 5,
-});
-
-// BURGER AND USER MENU
-
-document.getElementById("burger-btn").addEventListener("click", function (e) {
-  document.getElementById("user-menu-list-mob").classList.remove("visible");
-  document.getElementById("nav-mob").classList.toggle("visible");
-});
-
-document
-  .getElementById("user-menu-btn")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    document.getElementById("nav-mob").classList.remove("visible");
-    document.getElementById("user-menu-list-mob").classList.toggle("visible");
   });
 
-// CERTIFICATE
+  const swiperCertificate = new Swiper(".swiper-certificate", {
+    slidesPerView: 2.5,
+    spaceBetween: 10,
 
-if (document.getElementById("certificate")) {
+    breakpoints: {
+      567: {
+        spaceBetween: 20,
+        slidesPerView: 2.5,
+      },
+      767: {
+        spaceBetween: 20,
+        slidesPerView: 3.5,
+      },
+      991: {
+        spaceBetween: 20,
+        slidesPerView: 4.5,
+      },
+    },
+  });
+
+  const swiperTabs = new Swiper(".swiper-tabs", {
+    slidesPerView: "auto",
+    spaceBetween: 5,
+  });
+
+  // BURGER AND USER MENU
+
+  document.getElementById("burger-btn").addEventListener("click", function (e) {
+    document.getElementById("user-menu-list-mob").classList.remove("visible");
+    document.getElementById("nav-mob").classList.toggle("visible");
+  });
+
   document
-    .getElementById("certificate")
+    .getElementById("user-menu-btn")
     .addEventListener("click", function (e) {
-      if (e.target.classList.contains("certificate-img-js")) {
-        document.getElementById("modal-certificate-img").src =
-          e.target.dataset.src;
-        document.getElementById("modal").classList.add("visible");
-        document.body.classList.add("overflow-hidden");
-      }
+      e.preventDefault();
+      document.getElementById("nav-mob").classList.remove("visible");
+      document.getElementById("user-menu-list-mob").classList.toggle("visible");
     });
-}
-// MODAL
 
-document.getElementById("modal").addEventListener("click", function (e) {
-  if (
-    e.target.classList.contains("modal-close-js") ||
-    e.target.classList.contains("modal-bg-js")
-  ) {
-    this.classList.remove("visible");
-    document.body.classList.remove("overflow-hidden");
+  // CERTIFICATE
+
+  if (document.getElementById("certificate")) {
+    document
+      .getElementById("certificate")
+      .addEventListener("click", function (e) {
+        if (e.target.classList.contains("certificate-img-js")) {
+          openModal(e);
+        }
+      });
+  }
+  // MODAL
+
+  document.getElementById("modal").addEventListener("click", function (e) {
+    if (
+      e.target.classList.contains("modal-close-js") ||
+      e.target.classList.contains("modal-bg-js")
+    ) {
+      this.classList.remove("visible");
+      document.body.classList.remove("overflow-hidden");
+    }
+  });
+
+  // ACCORDION
+
+  if (document.querySelector(".accordion")) {
+    document
+      .querySelectorAll(".accordion-panel.active")
+      .forEach(function (panel) {
+        panel.style.height = `${
+          panel.querySelector(".accordion-hidden").clientHeight
+        }px`;
+      });
+
+    document
+      .querySelector(".accordion")
+      .addEventListener("click", function (e) {
+        if (e.target.classList.contains("accordion-trigger-action")) {
+          e.target.parentElement.classList.toggle("active");
+
+          if (e.target.parentElement.classList.contains("active")) {
+            e.target.parentElement.nextElementSibling.style.height = `${
+              e.target.parentElement.nextElementSibling.querySelector(
+                ".accordion-hidden"
+              ).clientHeight
+            }px`;
+          } else {
+            e.target.parentElement.nextElementSibling.style.height = "0px";
+          }
+        } else if (e.target.classList.contains("certificate-img-js")) {
+          openModal(e);
+        }
+      });
+  }
+
+  function openModal(e) {
+    document.getElementById("modal-certificate-img").src = e.target.dataset.src;
+    document.getElementById("modal").classList.add("visible");
+    document.body.classList.add("overflow-hidden");
   }
 });
